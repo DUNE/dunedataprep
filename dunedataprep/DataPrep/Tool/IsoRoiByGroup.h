14d4311bd3f65aa7c1c6f59f35f68baf3c8c2291
Vyacheslav Galymov <vgalymov@ipnl.in2p3.fr>
2023-03-15 17:53:37 +0100

    updated

--- dunedataprep/DataPrep/Tool/IsoRoiByGroup.h
+++ dunedataprep/DataPrep/Tool/IsoRoiByGroup.h
@@ -3,15 +3,29 @@
 // Vyacheslav Galymov
 // March, 2023
 //
-// Select only sufficiently isolated ROIs in a given group channels
+// Select only sufficiently isolated ROIs in a given group of channels
 // Channel groups are obtained from the channel group tool channelGroups.
 //
+// The isolated deposits are selected based on ROI isolation based on
+// NchEz (ch exclusion zone) and NtickEz (tick exclusion zone) options
+// For a given channel ch the exclusion zone is defined within +/- NchEz
+// window. Within this window an ROI is included as isolated if it is 
+// separated by more than NtickEz from another ROIs. 
+// This makes an underlying assumptions that the channels in given range 
+// a continuous. However, their geographical position with respect to each 
+// other is not respected in this simple treatement. Example:
+//   - a channel N+1 might be physically removed from channel N by a 
+//     substantial distance but its ROIs will be masked. 
+//   - a channel M might be quite far from N in terms nch_ez window,
+//     but quite close physically (example collection strips in VD CRPs).
+//     In this case ROIs (due to end of the tracks) might be flagged as 
+//     isolated deposits
 //
 // Configuration:
 //   LogLevel: Log frequency: 0=none, 1=initialization, 2=every event
 //   Groups: List of group names.
-//   Options: List of options (last overrides):
-//    
+//   NChanEz : Number of channels to define an exclusion zone -> m_NchEz
+//   NTickEz : Number of ticks to define an exclusion zoe -> m_NtickEz
 //    
 
 #ifndef IsoRoiByGroup_H
@@ -49,7 +63,11 @@ private:
   // Configuration data.
   int  m_LogLevel;
   NameVector m_Groups;
-  NameVector m_Options;
+  Index m_NchEz;
+  Index m_NtickEz;
+  
+  //
+  ChannelGroups m_chg;
 
 };
 
--- dunedataprep/DataPrep/Tool/IsoRoiByGroup_tool.cc
+++ dunedataprep/DataPrep/Tool/IsoRoiByGroup_tool.cc
@@ -10,15 +10,69 @@
 using std::string;
 using std::cout;
 using std::endl;
+using std::setw;
 
 //
-//
+// Ctor
 IsoRoiByGroup::IsoRoiByGroup(fhicl::ParameterSet const& ps)
 : m_LogLevel(ps.get<int>("LogLevel")),
   m_Groups(ps.get<NameVector>("Groups")),
-  m_Options(ps.get<NameVector>("Options")) {
-  const string myname = "IsoRoiByGroup::ctor: ";
+  m_NchEz(ps.get<Index>("NChanEz")),
+  m_NtickEz(ps.get<Index>("NTickEz")) {
   
+  //
+  const string myname = "IsoRoiByGroup::ctor: ";
+
+  // Build the channel map.
+  string crtName = "channelGroups";
+  DuneToolManager* ptm = DuneToolManager::instance();
+  const IndexRangeGroupTool* pcrt = ptm == nullptr ? nullptr : ptm->getShared<IndexRangeGroupTool>(crtName);
+  int nerr = 0;
+  for ( Name sgrp : m_Groups ) {
+    IndexRangeGroup grp;
+    if ( pcrt != nullptr ) grp = pcrt->get(sgrp);
+    if ( ! grp.isValid() ) {
+      grp = IndexRangeGroup(sgrp);
+    }
+    if ( ! grp.isValid() || grp.name.size() == 0 ) {
+      cout << myname << "WARNING: Unable to find range group " << sgrp << endl;
+      ++nerr;
+    } else {
+      grp.getIndices(m_chg[grp.name]);
+    }
+  }
+  if ( nerr ) {
+    if ( pcrt == nullptr ) {
+      cout << myname << "Channel groups tool " << crtName << " was not found." << endl;
+    } else {
+      cout << myname << "Channel groups tool " << crtName << " listing:" << endl;
+      pcrt->get("list");
+    }
+  }
+
+  // Log the configuration.
+  if ( m_LogLevel >= 1 ) {
+    cout << myname << "  LogLevel: " << m_LogLevel << endl;
+    cout << myname << "    Groups: [";
+    int count = 0;
+    for ( Name nam : m_Groups ) {
+      if ( count && count%10 == 0 ) cout << "\n" + myname + "             ";
+      if ( count++ ) cout << ", ";
+      cout << nam;
+    }
+    cout << "]" << endl;
+    
+    if ( m_chg.size() ) {
+      cout << myname << "     Group   #chan" << endl;
+      for ( const auto& ient : m_chg ) {
+        cout << myname << setw(10) << ient.first << setw(8) << ient.second.size() << endl;
+      }
+    } else {
+      cout << myname << "No channel groups found." << endl;
+    }
+    cout<<myname<<"Channel exclusion zone : "<<m_NchEz<<endl;
+    cout<<myname<<"Tick exclusion zone    : "<<m_NtickEz<<endl;
+  }
 }
 
 //
@@ -36,3 +90,6 @@ DataMap IsoRoiByGroup::updateMap(AdcChannelDataMap& acds) const {
   
   return ret;
 }
+
+
+DEFINE_ART_CLASS_TOOL(IsoRoiByGroup)
